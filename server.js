bfd28555ff1b9239c6adf4af42d842384c74a274
Elizabeth Asbell <e.asbell8@gmail.com>
2019-04-03 14:47:06 -0600

    Change naming issues.

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ app.use(cors());
 
 app.locals.notes = [];
 // app.locals.notes = [
-//     {id: shortid.generate(), title: 'gym', noteItems:['suana', 'swim', 'yoga']},
-//     {id: shortid.generate(), title: 'groceries', noteItems:['apple sauce']},
-//     {id: shortid.generate(), title: 'study', noteItems:['C++', 'simple math']}
+//   {id: shortid.generate(), title: 'gym', noteItems:['suana', 'swim', 'yoga']},
+//   {id: shortid.generate(), title: 'groceries', noteItems:['apple sauce']},
+//   {id: shortid.generate(), title: 'study', noteItems:['C++', 'simple math']}
 // ]
 
 app.listen('3001', () => {
@@ -29,21 +29,21 @@ app.get('/api/notes/:id', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-    const { title, noteItems } = req.body;
-    const id = shortid.generate();
-    const newNote = { id, title, noteItems };
-    if (!id || !title) return res.status(422).json({ error: 'Error posting note.' });
-    app.locals.notes.push(newNote);
-    res.status(201).json(newNote);
+  const { title, itemsList } = req.body;
+  const id = shortid.generate();
+  const newNote = { id, title, itemsList };
+  if ( !id || !title) return res.status(422).json({ error: 'Error posting note.' });
+  app.locals.notes.push(newNote);
+  res.status(201).json(newNote);
 });
 
 app.put('/api/notes/:id', (req, res) => {
-    const { id } = req.params;
-    const note = req.body;
-    const noteIdx = app.locals.notes.findIndex(note => note.id == id);
-    if (noteIdx === -1) return res.status(404).json('Note not found.');
-    app.locals.notes.splice(noteIndex, 1, note);
-    return res.status(200).json(app.locals.notes[noteIdx]);
+  const { id } = req.params;
+  const note = req.body;
+  const noteIdx = app.locals.notes.findIndex(note => note.id == id);
+  if (noteIdx === -1) return res.status(404).json('Note not found.');
+  app.locals.notes.splice(noteIndex, 1, note);
+  return res.status(200).json(app.locals.notes[noteIdx]);
 });
 
 app.delete('/api/notes/:id', (req, res) => {
